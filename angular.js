3772e57681176697099d71e595c27c5ad47986d7
iCrawl <buechler.noel@outlook.com>
2021-12-06 13:29:48 +0100

    fix: angular setup files

--- angular.js
+++ angular.js
@@ -1,24 +1,27 @@
 const path = require('path');
 
 module.exports = {
-	extends: ['plugin:@angular-eslint/recommended', path.join(__dirname, 'index.js')],
 	overrides: [
+		{
+			files: ['*.ts'],
+			extends: ['plugin:@angular-eslint/recommended', path.join(__dirname, 'index.js')],
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': [
+					'error',
+					{
+						allowExpressions: true,
+					},
+				],
+				'@typescript-eslint/naming-convention': 0,
+				'@typescript-eslint/no-floating-promises': 0,
+				'@typescript-eslint/no-extraneous-class': 0,
+				'@typescript-eslint/no-empty-function': 0,
+			},
+		},
 		{
 			files: ['*.html'],
 			extends: ['plugin:@angular-eslint/template/recommended'],
 			rules: {},
 		},
 	],
-	rules: {
-		'@typescript-eslint/explicit-function-return-type': [
-			'error',
-			{
-				allowExpressions: true,
-			},
-		],
-		'@typescript-eslint/naming-convention': 0,
-		'@typescript-eslint/no-floating-promises': 0,
-		'@typescript-eslint/no-extraneous-class': 0,
-		'@typescript-eslint/no-empty-function': 0,
-	},
 };
